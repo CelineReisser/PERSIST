a0e18921306e919951b86518d28a6f18582ae1f2
Celine M.O. Reisser <celine.reisser@gmail.com>
2020-04-02 10:16:56 +0200

    Update 09b_vcftools_remove_miss_Ind.sh

--- 00_scripts/09b_vcftools_remove_miss_Ind.sh
+++ 00_scripts/09b_vcftools_remove_miss_Ind.sh
@@ -11,4 +11,4 @@ VCFTOOLSENV=""
 $VCFTOOLSENV
 cd $WORKDIR
 
-vcftools --remove-indv blanc2 --remove-indv blanc1 --remove-indv C1_3 --remove-indv C1_2 --remove-indv S6_6 --remove-indv C1_5 --remove-indv S2_5 --remove-indv S2_24.bis --remove-indv C4_4 --recode --vcf $INDIR/DP10_SNP_MAF0.01_miss0.1_PERSIST_freebayes_stringent_biallelic.vcf --out $INDIR/DP10_SNP_MAF0.01_miss0.1_PERSIST_freebayes_stringent_biallelic_IndFiltered &> 98_log_files/10b_vcftools_remove_missInd.txt
+vcftools --remove-indv blanc2 --remove-indv blanc1 --remove-indv C1_3 --remove-indv C1_2 --remove-indv N6_6 --remove-indv C1_5 --remove-indv C4_4 --recode --vcf $INDIR/DP10_SNP_MAF0.01_miss0.1_PERSIST_freebayes_stringent_biallelic.vcf --out $INDIR/DP10_SNP_MAF0.01_miss0.1_PERSIST_freebayes_stringent_biallelic_IndFiltered &> 98_log_files/10b_vcftools_remove_missInd.txt
